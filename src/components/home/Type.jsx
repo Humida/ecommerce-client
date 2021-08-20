4196acd71e6a7a03835a995f0f2f89de49a588ec
Humida <huynhminhdanh1110@gmail.com>
2021-08-20 07:08:53 +0700

    responsive

--- src/components/home/Type.jsx
+++ src/components/home/Type.jsx
@@ -5,22 +5,19 @@ function Type(props) {
   return (
     <div id="type">
       <div className="type__container">
-        <TypeItem />
-        <TypeItem />
-        <TypeItem />
+        <TypeItem url="https://images.unsplash.com/photo-1519554318711-aaf73ece6ff9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
+        <TypeItem url="https://images.unsplash.com/photo-1585914924626-15adac1e6402?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80" />
+        <TypeItem url="https://images.unsplash.com/photo-1610906597362-174c5a3fa7d7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=656&q=80" />
       </div>
     </div>
   );
 }
 
 function TypeItem(props) {
+  const url = props.url;
   return (
     <div className="type__item">
-      <img
-        src="https://images.unsplash.com/photo-1624571032416-f86c480bf2b0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80"
-        alt=""
-        className="type__item-image"
-      />
+      <img src={url} alt="" className="type__item-image" />
       <h2 className="type__item-title">Shirt</h2>
       <p className="type__item-more">More</p>
       <div className="overlay"></div>
